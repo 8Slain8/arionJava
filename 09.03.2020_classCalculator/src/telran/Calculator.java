b638bb3f96785bb6a5088ffa3300eee42c00ac5f
8Slain8 <arion_sinagra@outlook.com>
2020-03-09 20:26:36 +0100

    calc + dialog

--- 09.03.2020_classCalculator/src/telran/Calculator.java
+++ 09.03.2020_classCalculator/src/telran/Calculator.java
@@ -1,27 +1,12 @@
 package telran;
 
 import java.util.Scanner;
+import static telran.Dialog.*;
 
 public class Calculator {
-        static Scanner scanner = new Scanner(System.in);
-        public static double getParameter() {
-            double a = scanner.nextDouble();
-            return a;
-        }
-        public static int operationChoice(){
-            System.out.println("Enter operation symbol");
-            System.out.println("1 - Addition");
-            System.out.println("2 - Subtraction");
-            System.out.println("3 - Multiplication");
-            System.out.println("4 - Division");
-            int operation = scanner.nextInt();
-            while(operation<1 || operation>4){
-                System.out.println("Error. Please enter correct function number");
-                operation = scanner.nextInt();
-            }
-            return operation;
-        }
+
         public static void calculation(double a, double b, int operation){
+            Scanner scanner = new Scanner(System.in);
         double result;
         if(operation==1){
             result = a+b;
@@ -38,7 +23,9 @@ public class Calculator {
                 }else{
                     result = a/b;
                 }
+        }
+
             System.out.println("Result is: " + result);
-            }
+            scanner.close();
         }
 }
--- 09.03.2020_classCalculator/src/telran/Main.java
+++ 09.03.2020_classCalculator/src/telran/Main.java
@@ -3,7 +3,7 @@ package telran;
 import java.util.Scanner;
 
 import static telran.Calculator.*;
-
+import static telran.Dialog.*;
 
 public class Main {
     public static void main(String[] args) {
